d582849529dd0f05465d0edfc0fed4db8ded4786
Sneha Santoshkumar (Student) <sneha.santoshkumar.2022@uni.strath.ac.uk>
2023-07-27 15:10:03 +0100

    fetching data from indian stock api

--- get_sock_data.py
+++ get_sock_data.py
@@ -1,16 +1,34 @@
 import pandas as pd
 import yfinance as yf
+from bsedata.bse import BSE
+from NSEDownload import stocks
+
 
 def get_ticker(country):
     if country == 'India':
-        df = pd.read_csv('data/indian_stock.csv')
-        return df['Security Id'].tolist()
+        df = pd.read_csv('data/nse_stocks.csv')
+        return df['SYMBOL'].tolist()
     elif country == 'USA':
         df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/s-and-p-500-companies/master/data/constituents_symbols.txt')
         return df
 
-def get_indian_stock(ticker,startDate,endDate):
-    pass
+def get_scripcode(tickerSymbol):
+    df = pd.read_csv('data/indian_stock.csv')
+    return df[df['Security Id'] == tickerSymbol]['Security Code'].tolist()[0]
+def get_indian_stock(tickerSymbol,startDate,endDate):
+    # scripcode = get_scripcode(tickerSymbol)
+    print(str(startDate),endDate,tickerSymbol)
+    df = stocks.get_data(stock_symbol=tickerSymbol, start_date=str(startDate), end_date=str(endDate))
+    return df
+
+
+def get_history(tickerSymbol, periodd):
+    scripcode =get_scripcode(tickerSymbol)
+    print(type(str(scripcode)))
+    b = BSE()
+    his = dict(b.getPeriodTrend(str(scripcode), periodd))
+    print(his)
+    return his
 
 def get_usa_stock(tickerSymbol,start_date,end_date):
     tickerData = yf.download(tickerSymbol, period='1d', start=start_date, end=end_date)  # Get ticker data
--- stock.py
+++ stock.py
@@ -8,105 +8,136 @@ import matplotlib.pyplot as plt
 import numpy as np
 import plotly.express as px
 from dotenv import load_dotenv
+from alpha_vantage.fundamentaldata import FundamentalData
+from stocknews import StockNews
+import pandas_ta as ta
+
 
 import os
-from get_sock_data import get_ticker, get_indian_stock, get_usa_stock
+from get_sock_data import get_ticker, get_indian_stock, get_usa_stock, get_history, get_scripcode
 load_dotenv(".env")
 
 ALPHA_VANTAGE_API_KEY = os.getenv("ALPHA_VANTAGE_KEY")
 # select country
 country = st.sidebar.radio('Country', ['USA', 'India'],horizontal=True)
 tickerSymbol = st.sidebar.selectbox("Select Stock", get_ticker(country))
-if country == 'India':
-    pass
 
 start_date = st.sidebar.date_input("Start date", datetime.date.today() - datetime.timedelta(days=30),
                                        max_value=datetime.date.today())
 end_date = st.sidebar.date_input("End date", datetime.date.today(), max_value=datetime.date.today())
-tickerData, tickerData2 = get_usa_stock(tickerSymbol, start_date, end_date)
 
-show_ticker_info = st.sidebar.checkbox("Show Ticker Information")
-
-
-
-string_name = tickerData2.info['longName']
-st.header('**%s**' % string_name)
-
-# Ticker information
-if show_ticker_info:
-    string_summary = tickerData2.info['longBusinessSummary']
-    st.info(string_summary)
-
-fig= px.line(tickerData, x=tickerData.index, y=tickerData['Adj Close'], title=f'{tickerSymbol} Stock Price')
-st.plotly_chart(fig)
-
-st.header('**Bollinger Bands**')
-qf=cf.QuantFig(tickerData,title='First Quant Figure',legend='top',name='GS')
-qf.add_bollinger_bands()
-fig = qf.iplot(asFigure=True)
-st.plotly_chart(fig)
-
-pricing_data, fundamental_data, news, tech_indicator = st.tabs(["Pricing Data", "Fundamental Data", "News", "Technical Analysis"])
-
-with pricing_data:
-    st.header('Pricing Movements')
-    data2= tickerData
-    data2['% Change'] = tickerData['Adj Close']/tickerData['Adj Close'].shift(1)-1
-    data2.dropna(inplace=True)
-    st.write(data2)
-    annual_return = data2['% Change'].mean() * 252 * 100
-    st.write('Annual return : ', annual_return, '%')
-    stdev = np.std(data2['% Change']) * np.sqrt(252) * 100
-    st.write('Standard Deviation : ', stdev, '%')
-    st.write('Risk Adj return : ', annual_return/stdev)
+if country == 'India':
+    indianTickerData = get_indian_stock(tickerSymbol,startDate=start_date,endDate=end_date)
+    st.dataframe(indianTickerData)
+    st.header('**Bollinger Bands**')
+    qf=cf.QuantFig(indianTickerData,title='First Quant Figure',legend='top',name='GS')
+    qf.add_bollinger_bands()
+    fig = qf.iplot(asFigure=True)
+    st.plotly_chart(fig)
+    st.header('**MACD**')
+    qf=cf.QuantFig(indianTickerData,title='First Quant Figure',legend='top',name='GS')
+    qf.add_macd()
+    fig = qf.iplot(asFigure=True)
+    st.plotly_chart(fig)
+    st.header('**RSI**')
+    qf=cf.QuantFig(indianTickerData,title='First Quant Figure',legend='top',name='GS')
+    qf.add_rsi()
+    fig = qf.iplot(asFigure=True)
+    st.plotly_chart(fig)
+    st.header('**EMA**')
+    qf=cf.QuantFig(indianTickerData,title='First Quant Figure',legend='top',name='GS')
+    qf.add_ema()
+    fig = qf.iplot(asFigure=True)
+    st.plotly_chart(fig)
+    st.header('**SMA**')
+    qf=cf.QuantFig(indianTickerData,title='First Quant Figure',legend='top',name='GS')
+    qf.add_sma()
+    fig = qf.iplot(asFigure=True)
+    st.plotly_chart(fig)
 
 
+else:
+    tickerData, tickerData2 = get_usa_stock(tickerSymbol, start_date, end_date)
 
-from alpha_vantage.fundamentaldata import FundamentalData
-with fundamental_data:
-    key = ALPHA_VANTAGE_API_KEY
-    fd = FundamentalData(key, output_format='pandas')
-    st.subheader('Balance Sheet')
-    balance_sheet = fd.get_balance_sheet_annual(tickerSymbol)[0]
-    bs= balance_sheet.T[2:]
-    bs.columns= list(balance_sheet.T.iloc[0])
-    st.write(bs)
-    st.subheader('Income Statement')
-    income_statement = fd.get_income_statement_annual(tickerSymbol)[0]
-    ist= income_statement.T[2:]
-    ist.columns= list(income_statement.T.iloc[0])
-    st.write(ist)
-    st.subheader('Cash Flow Statement')
-    cash_flow = fd.get_cash_flow_annual(tickerSymbol)[0]
-    cf= cash_flow.T[2:]
-    cf.columns= list(cash_flow.T.iloc[0])
-    st.write(cf)
+    show_ticker_info = st.sidebar.checkbox("Show Ticker Information")
 
-from stocknews import StockNews
-with news:
-    st.header(f'News of {tickerSymbol}')
-    sn= StockNews(tickerSymbol, save_news=False)
-    df_news= sn.read_rss()
-    for i in range(10):
-        st.subheader(f'News {i+1}')
-        st.write(df_news['published'][i])
-        st.write(df_news['title'][i])
-        st.write(df_news['summary'][i])
-        title_sentiment = df_news['sentiment_title'][i]
-        st.write(f'Title Sentiment : {title_sentiment}')
-        news_sentiment = df_news['sentiment_summary'][i]
-        st.write(f'News Sentiment : {news_sentiment}')
 
-import pandas_ta as ta
-with tech_indicator:
-    st.subheader('Technical Analysis')
-    df= pd.DataFrame()
-    ind_list= df.ta.indicators(as_list=True)
-    technical_indicator= st.selectbox('Select Technical Indicator', options=ind_list)
-    method= technical_indicator
-    indicator= pd.DataFrame(getattr(ta, method)(low=tickerData['Low'], high=tickerData['High'], close=tickerData['Close'], volume=tickerData['Volume']))
-    indicator['Close']= tickerData['Close']
-    fig_ind= px.line(indicator)
-    st.plotly_chart(fig_ind)
-    st.write(indicator)
+
+    string_name = tickerData2.info['longName']
+    st.header('**%s**' % string_name)
+
+    # Ticker information
+    if show_ticker_info:
+        string_summary = tickerData2.info['longBusinessSummary']
+        st.info(string_summary)
+
+    fig= px.line(tickerData, x=tickerData.index, y=tickerData['Adj Close'], title=f'{tickerSymbol} Stock Price')
+    st.plotly_chart(fig)
+
+    st.header('**Bollinger Bands**')
+    qf=cf.QuantFig(tickerData,title='First Quant Figure',legend='top',name='GS')
+    qf.add_bollinger_bands()
+    fig = qf.iplot(asFigure=True)
+    st.plotly_chart(fig)
+
+    pricing_data, fundamental_data, news, tech_indicator = st.tabs(["Pricing Data", "Fundamental Data", "News", "Technical Analysis"])
+
+    with pricing_data:
+        st.header('Pricing Movements')
+        data2= tickerData
+        data2['% Change'] = tickerData['Adj Close']/tickerData['Adj Close'].shift(1)-1
+        data2.dropna(inplace=True)
+        st.write(data2)
+        annual_return = data2['% Change'].mean() * 252 * 100
+        st.write('Annual return : ', annual_return, '%')
+        stdev = np.std(data2['% Change']) * np.sqrt(252) * 100
+        st.write('Standard Deviation : ', stdev, '%')
+        st.write('Risk Adj return : ', annual_return/stdev)
+
+
+
+    with fundamental_data:
+        key = ALPHA_VANTAGE_API_KEY
+        fd = FundamentalData(key, output_format='pandas')
+        st.subheader('Balance Sheet')
+        balance_sheet = fd.get_balance_sheet_annual(tickerSymbol)[0]
+        bs= balance_sheet.T[2:]
+        bs.columns= list(balance_sheet.T.iloc[0])
+        st.write(bs)
+        st.subheader('Income Statement')
+        income_statement = fd.get_income_statement_annual(tickerSymbol)[0]
+        ist= income_statement.T[2:]
+        ist.columns= list(income_statement.T.iloc[0])
+        st.write(ist)
+        st.subheader('Cash Flow Statement')
+        cash_flow = fd.get_cash_flow_annual(tickerSymbol)[0]
+        cf= cash_flow.T[2:]
+        cf.columns= list(cash_flow.T.iloc[0])
+        st.write(cf)
+
+    with news:
+        st.header(f'News of {tickerSymbol}')
+        sn= StockNews(tickerSymbol, save_news=False)
+        df_news= sn.read_rss()
+        for i in range(10):
+            st.subheader(f'News {i+1}')
+            st.write(df_news['published'][i])
+            st.write(df_news['title'][i])
+            st.write(df_news['summary'][i])
+            title_sentiment = df_news['sentiment_title'][i]
+            st.write(f'Title Sentiment : {title_sentiment}')
+            news_sentiment = df_news['sentiment_summary'][i]
+            st.write(f'News Sentiment : {news_sentiment}')
+
+    with tech_indicator:
+        st.subheader('Technical Analysis')
+        df= pd.DataFrame()
+        ind_list= df.ta.indicators(as_list=True)
+        technical_indicator= st.selectbox('Select Technical Indicator', options=ind_list)
+        method= technical_indicator
+        indicator= pd.DataFrame(getattr(ta, method)(low=tickerData['Low'], high=tickerData['High'], close=tickerData['Close'], volume=tickerData['Volume']))
+        indicator['Close']= tickerData['Close']
+        fig_ind= px.line(indicator)
+        st.plotly_chart(fig_ind)
+        st.write(indicator)
 

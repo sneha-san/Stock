0176f2e783f3fc7592b35eea4afab129af2f48ed
Sneha Santoshkumar (Student) <sneha.santoshkumar.2022@uni.strath.ac.uk>
2023-07-28 18:03:08 +0100

    final commit before push

--- get_sock_data.py
+++ get_sock_data.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import yfinance as yf
-from bsedata.bse import BSE
 from NSEDownload import stocks
 
 
@@ -12,23 +11,16 @@ def get_ticker(country):
         df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/s-and-p-500-companies/master/data/constituents_symbols.txt')
         return df
 
-def get_scripcode(tickerSymbol):
-    df = pd.read_csv('data/indian_stock.csv')
-    return df[df['Security Id'] == tickerSymbol]['Security Code'].tolist()[0]
+def get_ticker_name(tickerSymbol):
+    df = pd.read_csv('data/nse_stocks.csv')
+    return df[df['SYMBOL'] == tickerSymbol]['NAME OF COMPANY'].tolist()[0]
+
 def get_indian_stock(tickerSymbol,startDate,endDate):
-    # scripcode = get_scripcode(tickerSymbol)
-    print(str(startDate),endDate,tickerSymbol)
     df = stocks.get_data(stock_symbol=tickerSymbol, start_date=str(startDate), end_date=str(endDate))
     return df
 
 
-def get_history(tickerSymbol, periodd):
-    scripcode =get_scripcode(tickerSymbol)
-    print(type(str(scripcode)))
-    b = BSE()
-    his = dict(b.getPeriodTrend(str(scripcode), periodd))
-    print(his)
-    return his
+
 
 def get_usa_stock(tickerSymbol,start_date,end_date):
     tickerData = yf.download(tickerSymbol, period='1d', start=start_date, end=end_date)  # Get ticker data
--- newsdata.py
+++ newsdata.py
@@ -5,15 +5,10 @@ load_dotenv(".env")
 
 api_key = os.getenv("NEWS_DATA")
 
-# API key authorization, Initialize the client with your API key
-
 api = NewsDataApiClient(apikey=api_key)
 
-# You can pass empty or with request parameters {ex. (country = "us")}
-
 def get_news(query ,category,language, country):
-    response = api.news_api(category=category,language=language ,q=query,qInTitle=None, country = country)
+    response = api.news_api(category=category,language=language,q=query,qInTitle=None, country = country)
     return response
 
-response = get_news(category="business",language="en",query='Bajaj Finance Limited',country="in")
-print(response)
+
